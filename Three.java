b0c4aeecac425f2c3b11325ed641ae66d00df361
alexanderpromel <alexanderpromel@gmail.com>
2022-07-02 10:34:26 +0530

    new

---
+++ Three.java
@@ -0,0 +1,14 @@
+package org.lily;
+
+public class Three {
+	public void flowerPot() {
+		for(int i=0; i <28; i++) {
+			System.out.println("Flower pot");
+		}
+	}
+	public static void main(String[]args) {
+		Three t = new Three();
+		t.flowerPot();
+	}
+
+}
